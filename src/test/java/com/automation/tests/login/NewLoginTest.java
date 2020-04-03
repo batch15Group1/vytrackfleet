0ebd57463308ee28e77fca64d11dfb9d5f8e37a6
abrahamhope <abrahamisaachope@gmail.com>
2020-04-02 19:07:21 -0500

    vytrack first team commit

---
+++ src/test/java/com/automation/tests/login/NewLoginTest.java
@@ -0,0 +1,36 @@
+package com.automation.tests.login;
+
+import com.automation.pages.LoginPage;
+import com.automation.tests.vytrack.AbstractTestBase;
+import com.automation.utilities.Driver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class NewLoginTest extends AbstractTestBase {
+
+    @Test
+    public void verifyPageTitle(){
+        //test --> ExtentTest object
+        //we must add to every test at the beginning
+        //test = report.createTest("Test name");
+        test = report.createTest("Verify page title");
+        LoginPage loginPage = new LoginPage();
+        loginPage.login();
+        Assert.assertEquals(Driver.getDriver().getTitle(), "Dashboard");
+        //if assertion passed, it will set test status in report to passed
+        test.pass("Page title Dashboard was verified");
+    }
+
+
+
+    /**
+     * Enter
+     */
+    @Test
+    public void verifyWarningMessage(){
+       LoginPage loginPage = new LoginPage();
+       loginPage.login("wrong", "wrong");
+       Assert.assertEquals(loginPage.getWarningMessageText(),"Invalid user name or password.");
+
+    }
+}
