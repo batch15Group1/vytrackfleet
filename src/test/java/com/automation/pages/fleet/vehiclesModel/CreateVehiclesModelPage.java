bffba3c103d3d87c555feb19ca35059ac2939c0f
abrahamhope <abrahamisaachope@gmail.com>
2020-04-07 10:23:44 -0500

    added createVehiclesModelPage.java

--- src/test/java/com/automation/pages/fleet/vehiclesModel/CreateVehiclesModelPage.java
+++ src/test/java/com/automation/pages/fleet/vehiclesModel/CreateVehiclesModelPage.java
@@ -7,12 +7,21 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class CreateVehiclesModelPage extends AbstractPageBase {
-@FindBy(css = "[id^='custom_entity_type_ModelName-uid']")
+    @FindBy(css = "[id^='custom_entity_type_ModelName-uid']")
     private WebElement modelName;
 
+    @FindBy(css = "[id^='custom_entity_type_make-uid']")
+    private WebElement make;
 
-public void enterModelName(String modelNameValue){
-    BrowserUtils.waitForPageToLoad(20);
-    wait.until(ExpectedConditions.visibilityOf(modelName)).sendKeys(modelNameValue);
+
+    public void enterModelName(String modelNameValue) {
+        BrowserUtils.waitForPageToLoad(20);
+        wait.until(ExpectedConditions.visibilityOf(modelName)).sendKeys(modelNameValue);
+    }
+
+    public void enterMake(String makeValue) {
+        BrowserUtils.waitForPageToLoad(20);
+        wait.until(ExpectedConditions.visibilityOf(make)).sendKeys(makeValue);
+    }
 }
-}
+
