8107a9c4caff751daf2fa02f5e9137e3909dc444
Birnigar <farukgalatasaray34@gmail.com>
2020-04-07 17:11:53 -0700

    first commit

--- src/test/java/com/automation/tests/fleet/VehicleOdometerTests.java
+++ src/test/java/com/automation/tests/fleet/VehicleOdometerTests.java
@@ -1,5 +1,12 @@
 package com.automation.tests.fleet;
 
-public class VehicleOdometerTests {
+import com.automation.utilities.ConfigurationReader;
+import com.automation.utilities.Driver;
+import org.openqa.selenium.WebDriver;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 
+public class VehicleOdometerTests {
+}
 }
