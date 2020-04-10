828ee3fd29e0f101879dc0e448954053df3669bf
xiamuxinuer <emmaxiaogaiti@gmail.com>
2020-04-09 20:31:40 -0500

    emma added "access to VehiclesModelPage" test

--- src/test/java/com/automation/tests/fleet/VehiclesModelTests.java
+++ src/test/java/com/automation/tests/fleet/VehiclesModelTests.java
@@ -1,4 +1,22 @@
 package com.automation.tests.fleet;
 
-public class VehiclesModelTests {
+import com.automation.pages.fleet.VehiclesModelPage;
+import com.automation.pages.login.LoginPage;
+import com.automation.utilities.AbstractTestBase;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class VehiclesModelTests extends AbstractTestBase {
+
+
+ @Test
+ public void accessToVehicleModelPage(){
+     LoginPage loginPage=new LoginPage();
+     loginPage.login();
+     VehiclesModelPage vehiclesModelPage=new VehiclesModelPage();
+     vehiclesModelPage.navigateTo("Fleet","Vehicles Model");
+
+     Assert.assertTrue(vehiclesModelPage.getSubTitle().isDisplayed());
+ }
+
 }
