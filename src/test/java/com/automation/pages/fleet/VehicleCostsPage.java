7fd1a25309d8530f71c48f62f18c4937996794df
onuralpd <58891911+onuralpd@users.noreply.github.com>
2020-04-13 00:03:34 -0500

    Vehicle Costs Page US1 is Done

--- src/test/java/com/automation/pages/fleet/VehicleCostsPage.java
+++ src/test/java/com/automation/pages/fleet/VehicleCostsPage.java
@@ -1,4 +1,37 @@
 package com.automation.pages.fleet;
 
-public class VehicleCostsPage {
+import com.automation.utilities.AbstractPageBase;
+import com.automation.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class VehicleCostsPage extends AbstractPageBase {
+//    5.Click  Create Vehicle Cost button
+//    6.Put the informations
+//    7. Click Save button
+    @FindBy(className = "btn main-group btn-primary pull-right ")
+    private WebElement createVehicleCost;
+
+    @FindBy(className = "select2-chosen")
+    private WebElement selectType;
+    @FindBy(id = "custom_entity_type_TotalPrice-uid-5e93dcd32021f")
+    private WebElement totalPrice;
+    @FindBy(id = "date_selector_custom_entity_type_Date-uid-5e93dcd32030c")
+    private WebElement date;
+    @FindBy(id = "custom_entity_type_CostDescriptions-uid-5e93dcd3203f2")
+    private WebElement costDescription;
+
+    public void clickToCreateVehicleCost(){
+        WebDriverWait wait =new WebDriverWait(driver,10);
+        wait.until(ExpectedConditions.elementToBeClickable(createVehicleCost)).click();
+    }
+public void setActualResult() {
 }
+
+
+
+}
+
--- src/test/java/com/automation/tests/fleet/VehicleCostsTests.java
+++ src/test/java/com/automation/tests/fleet/VehicleCostsTests.java
@@ -1,5 +1,32 @@
 package com.automation.tests.fleet;
 
-public class VehicleCostsTests {
+import com.automation.pages.fleet.VehicleCostsPage;
+import com.automation.pages.login.LoginPage;
+import com.automation.utilities.AbstractTestBase;
+import com.automation.utilities.Driver;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
-}
+public class VehicleCostsTests extends AbstractTestBase {
+//UserStory #5 /AC #1 /Test Case #1
+    //1.Verify that truck driver should be able to see all cars' cost informations on the Vehicle Costs page.
+  //Test Case #1 Truck driver can see cost information
+//    5.Click  Create Vehicle Cost button
+//    6.Put the informations
+//    7. Click Save button
+    @Test
+    public void testCreateVehicleCost(){
+        LoginPage loginPage = new LoginPage();
+        VehicleCostsPage vehicleCostsPage = new VehicleCostsPage();
+
+        loginPage.login("User1","UserUser123");
+        vehicleCostsPage.navigateTo("Fleet","Vehicle Costs");
+        String actualResult = Driver.getDriver().findElement(By.className("oro-subtitle")).getText();
+        String expectedResult = "Vehicle Costs";
+
+        Assert.assertEquals(actualResult,expectedResult);
+
+        }
+    }
+
