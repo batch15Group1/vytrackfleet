0b4f18c51d08fdf425b7f9806bf979ccdc1a83da
Birnigar <farukgalatasaray34@gmail.com>
2020-04-16 17:35:25 -0700

    change

--- src/test/java/com/automation/tests/fleet/VehicleOdometerTests.java
+++ src/test/java/com/automation/tests/fleet/VehicleOdometerTests.java
@@ -1,23 +1,16 @@
 package com.automation.tests.fleet;
 
 import com.automation.pages.fleet.VehicleOdometerPage;
-import com.automation.pages.fleet.VehiclesPage;
 import com.automation.pages.login.LoginPage;
 import com.automation.utilities.AbstractTestBase;
-import com.automation.utilities.ConfigurationReader;
-import com.automation.utilities.Driver;
-import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
-import org.testng.ITestResult;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.util.List;
+
 
 public class VehicleOdometerTests extends AbstractTestBase {
 
-    protected WebDriver driver=Driver.getDriver();
+
 /**
  * Verify that Only truck driver and admin can access to vehicle odometer page
  * 1.Open the URL
