8806d2991c0ca2bff6263fd1b02d9a91a1eed40e
Birnigar <farukgalatasaray34@gmail.com>
2020-04-21 13:25:56 -0700

    driver create vehicle odometer

--- src/test/java/com/automation/pages/fleet/VehicleOdometerPage.java
+++ src/test/java/com/automation/pages/fleet/VehicleOdometerPage.java
@@ -1,8 +1,12 @@
 package com.automation.pages.fleet;
 
 import com.automation.utilities.AbstractPageBase;
+import com.automation.utilities.BrowserUtils;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 
 public class VehicleOdometerPage extends AbstractPageBase {
 
@@ -14,15 +18,80 @@ public class VehicleOdometerPage extends AbstractPageBase {
     @FindBy(xpath = "//div[@class='message' and text()='You do not have permission to perform this action.']")
     private WebElement warningMessage;
 
+    @FindBy(className = "grid-header")
+    private WebElement gridInformationTable;
+
+    @FindBy(partialLinkText = "Create Vehicle")
+    private WebElement  createVehicleOdometerButton;
+
+    @FindBy(xpath = "//input[@name='custom_entity_type[OdometerValue]']")
+    private WebElement odometerValue;
+
+    @FindBy(xpath = "//input[@class='datepicker-input hasDatepicker']")
+    private WebElement date;
+    //select class
+
+    @FindBy(className = "ui-datepicker-month")
+    private WebElement monthButton;
+
+    @FindBy(className = "ui-datepicker-year")
+    private WebElement yearButton;
+
+    @FindBy(xpath = "//input[@name='custom_entity_type[Driver]']")
+    private WebElement driver;
+
+    @FindBy(xpath = "//div//button [@class='btn btn-success action-button']")
+    private WebElement saveAndCloseButton;
+
+    @FindBy(xpath = "//a[text()='Entities']")
+    private WebElement entitiesTitle;
+
+
+
+
+
+
+
 
     public  String getTitleVehicleOdometer() {
-
         return vehicleOdometerTitle.getText();
     }
 
     public String getWarningMessageNone_AuthorizedUser(){
-
+         BrowserUtils.wait(6);
         return warningMessage.getText();
+    }
+
+    public String getHeaderInformationTable(){
+        BrowserUtils.waitForPageToLoad(20);
+       return gridInformationTable.getText();
+    }
+
+    public void createVehicleOdometer(String milesValue,String driverName,int month,String year,String day){
+        String xpathDay="//a[text()='"+day+"']";
+        wait.until(ExpectedConditions.elementToBeClickable(createVehicleOdometerButton)).click();
+        BrowserUtils.wait(10);
+        odometerValue.sendKeys(milesValue);
+
+        date.click();
+
+
+        Select selectMonth=new Select(monthButton);
+        selectMonth.selectByIndex(month);
+
+        Select selectYear=new Select(yearButton);
+        selectYear.selectByValue(year);
+
+        driver.findElement(By.xpath(xpathDay)).click();
+
+        driver.sendKeys(driverName);
+        BrowserUtils.wait(2);
+        saveAndCloseButton.submit();
+
+    }
+
+    public String getEntitiesText(){
+        return entitiesTitle.getText();
     }
 }
 
--- src/test/java/com/automation/tests/fleet/VehicleOdometerTests.java
+++ src/test/java/com/automation/tests/fleet/VehicleOdometerTests.java
@@ -3,6 +3,7 @@ package com.automation.tests.fleet;
 import com.automation.pages.fleet.VehicleOdometerPage;
 import com.automation.pages.login.LoginPage;
 import com.automation.utilities.AbstractTestBase;
+import com.automation.utilities.BrowserUtils;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -13,10 +14,6 @@ public class VehicleOdometerTests extends AbstractTestBase {
 
 /**
  * Verify that Only truck driver and admin can access to vehicle odometer page
- * 1.Open the URL
- * 2.Enter valid credentials and click LOG IN button
- * 3.Hover over "Fleet " module
- * 4. click "Vehicle Odometer"
  */
 @Test
 public void verifyTrackUserNavigate(){
@@ -37,6 +34,29 @@ public void verifyTrackUserNavigate(){
 
 }
 
+
+    /**
+     * Verify that Only truck driver can access to vehicle odometer page
+     * with invalid password
+     */
+
+@Test
+public void verifyTrackUserNavigateWithInvalidPassword(){
+    test=report.createTest("Verify truck driver can not navigate page with invalid password");
+    LoginPage loginPage = new LoginPage();
+    loginPage.login("user1","wrong" );
+    String actual=loginPage.getWarningMessageText();
+    String expected="Invalid user name or password.";
+    Assert.assertEquals(actual,expected);
+
+    test.pass("User con not navigate with invalid password");
+}
+
+    /**
+     * Verify that non autorized user Not able to access Vehicle odometer page
+     * as a sales manager
+     */
+
 @Test
 public void verifySalesManagerNavigate(){
     test = report.createTest("Verify salesmanager  driver can not navigate to vehicle odometer module");
@@ -54,6 +74,11 @@ public void verifySalesManagerNavigate(){
 
     test.pass("None_AuthorizedUser not be able to access vehicle odometer page");
 }
+
+    /**
+     * Verify that non autorized user Not able to access Vehicle odometer page
+     * as a store manager
+     */
 
     @Test
     public void verifyStoreManagerNavigate(){
@@ -73,5 +98,53 @@ public void verifySalesManagerNavigate(){
         test.pass("None_AuthorizedUser not be able to access vehicle odometer page");
     }
 
+    /**
+     * Verify that truck driver should be able to see all vehicle odometer informations
+     * on the grid.
+     */
+    @Test
+    public void verifyDriverSeeInformationOnGrid(){
+    LoginPage loginPage=new LoginPage();
+    VehicleOdometerPage vehicleOdometerPage=new VehicleOdometerPage();
+    loginPage.loginAsDriver();
+    vehicleOdometerPage.navigateTo("Fleet","Vehicle Odometer");
+
+    String actual= vehicleOdometerPage.getHeaderInformationTable();
+    String expected="ODOMETER VALUE\n" +
+            "DATE\n" +
+            "DRIVER\n" +
+            "UNIT\n" +
+            "MODEL";
+    Assert.assertEquals(actual,expected);
+
+
+
+    }
+
+    /**
+     * Verify that Truck driver should be able to create Vehicle odometer
+     */
+
+    @Test
+    public void verifyDriverCreateVehicleOdometer(){
+        test=report.createTest("Verify Truck driver create vehicle odometer");
+
+        LoginPage loginPage=new LoginPage();
+        VehicleOdometerPage vehicleOdometerPage=new VehicleOdometerPage();
+        loginPage.loginAsDriver();
+
+        vehicleOdometerPage.navigateTo("Fleet","Vehicle Odometer");
+        BrowserUtils.wait(3);
+
+        vehicleOdometerPage.createVehicleOdometer("120","newdriver",5,"2020","25");
+
+        String actual=vehicleOdometerPage.getEntitiesText();
+        String expected="Entities";
+
+        Assert.assertEquals(actual,expected);
+
+        test.pass("Truck driver created vehicle successfully ");
+
+    }
 }
 
